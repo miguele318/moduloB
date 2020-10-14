7c2cc202fecc6b5fa23c06ee2dcf0c1879f0fd07
Miguele <calambasm@unicauca.edu.co>
2020-10-13 23:50:39 -0500

    ... .

--- posgrados_unicauca_backend/settings.py
+++ posgrados_unicauca_backend/settings.py
@@ -93,12 +93,8 @@ WSGI_APPLICATION = 'posgrados_unicauca_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'BD_POSGRADOS',
-        'USER': 'root',
-        'PASSWOORD': '',
-        'HOST': 'localhost',
-        'PORT': 3306,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
