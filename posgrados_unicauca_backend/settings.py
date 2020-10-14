6180941f6419bea35537d5f8b633cb69e965f29e
Edinson7 <ejhidalgoc@unicauca.edu.co>
2020-10-13 22:52:33 -0500

    migracion mysql

--- posgrados_unicauca_backend/settings.py
+++ posgrados_unicauca_backend/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'posgrados_unicauca_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BD_POSGRADOS',
+        'USER': 'root',
+        'PASSWOORD': '',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
